3007599b0976d15a102d5aade7f4e6d57b7002fe
byoungd <pg98@vip.qq.com>
2022-01-25 13:27:07 +0800

    feat: create project

---
+++ src/children/red.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="red">
+    <micro-app name="red" url="http://localhost:3002/" :data="data">
+      <!-- destroy inline -->
+    </micro-app>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const data = ref({ from: '来自基座的初始化数据' })
+</script>
