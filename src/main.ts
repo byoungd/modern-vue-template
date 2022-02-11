a56f2a4c4727392d4420eb4151e3cb483dac6177
byoungd <pg98@vip.qq.com>
2022-02-11 09:34:05 +0800

    refactor: remove unused code

--- src/main.ts
+++ src/main.ts
@@ -12,48 +12,9 @@ import App from './App.vue'
 // register vue composition api globally
 import { ViteSSG } from 'vite-ssg'
 import generatedRoutes from 'virtual:generated-pages'
-import microApp from '@micro-zoe/micro-app'
 import { setupLayouts } from 'virtual:generated-layouts'
 
 const routes = setupLayouts(generatedRoutes)
-// @ts-ignore
-microApp.start({
-  lifeCycles: {
-    created() {
-      console.log('created 全局监听')
-    },
-    beforemount() {
-      console.log('beforemount 全局监听')
-    },
-    mounted() {
-      console.log('mounted 全局监听')
-    },
-    unmount() {
-      console.log('unmount 全局监听')
-    },
-    error() {
-      console.log('error 全局监听')
-    },
-  },
-  plugins: {
-    modules: {
-      react: [
-        {
-          loader(code: string, url: string) {
-            if (
-              process.env.NODE_ENV === 'development' &&
-              code.indexOf('sockjs-node') > -1
-            ) {
-              console.log('react17插件', url)
-              code = code.replace('window.location.port', '3002')
-            }
-            return code
-          },
-        },
-      ],
-    },
-  },
-})
 
 // https://github.com/antfu/vite-ssg
 export const createApp = ViteSSG(
