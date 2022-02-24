914fd16b112bb82065c4b0cb9b5ebb61f05c9935
byoungd <pg98@vip.qq.com>
2022-02-24 18:03:48 +0800

    feat(modern-vue): using tailwind

--- apps/modern-vue/src/App.vue
+++ apps/modern-vue/src/App.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-// https://github.com/vueuse/head
-// you can use this to manipulate the document head in any components,
-// they will be rendered correctly in the html results with vite-ssg
+import { useHead } from '@vueuse/head'
 useHead({
   title: 'Modern Vue',
   meta: [{ name: 'description', content: 'Opinionated Vite Starter Template' }],
---
+++ apps/modern-vue/tailwind.config.js
@@ -0,0 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Inter var"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
