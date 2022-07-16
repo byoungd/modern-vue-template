98741d50b258f0be1f24c2fe6690e41443ed2ef0
byoungd <pg98@vip.qq.com>
2022-07-16 21:12:09 +0800

    chore: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,30 +2,27 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    es2021: true,
     browser: true,
+    // defineProps and defineEmits generate no-undef warnings
+    'vue/setup-compiler-macros': true,
   },
+  parser: 'vue-eslint-parser',
   extends: [
     'eslint:recommended',
     'plugin:vue/vue3-essential',
     'plugin:vue/vue3-strongly-recommended',
-    '@vue/typescript/recommended',
-    '@vue/eslint-config-prettier',
-    '@vue/eslint-config-typescript',
     'prettier',
   ],
   parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
     ecmaVersion: 2020,
   },
   plugins: ['vue', 'prettier'],
   // add your custom rules here
   rules: {
-    'prettier/prettier': [
-      'error',
-      {},
-      {
-        usePrettierrc: true,
-      },
-    ],
+    'no-unused-vars': ['error', { vars: 'all', args: 'none', ignoreRestSiblings: true }],
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/ban-types': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
